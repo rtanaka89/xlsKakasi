dc7b07a863aaa98221b43d5ac8d44c8988060036
Ryoji Tanaka <ryoji.tanaka89@gmail.com>
2015-05-09 18:41:53 +0900

    routesにexcelを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'kakasi/' => 'kakasi#index'
   post 'kakasi/' => 'kakasi#index'
 
+  resources :excel
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
