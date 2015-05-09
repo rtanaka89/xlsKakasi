2ae0f804081126c7861e24e4dbbe126e5cf07dbe
Ryoji Tanaka <ryoji.tanaka89@gmail.com>
2015-05-09 17:01:32 +0900

    routesの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :kakasi
+  #resources :kakasi
+  get 'kakasi/' => 'kakasi#index'
+  post 'kakasi/' => 'kakasi#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
